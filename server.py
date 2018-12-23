f149720f0e18ffb4668f2bb1adcaa2562c486934
saykara <saykara@itu.edu.tr>
2018-12-23 16:52:07 +0300

    Added database initalization to homepage

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from database import DatabaseOperations
 app = Flask(__name__)
 
 
 @app.route('/',  methods=['GET', 'POST'])
 def main_page():
-    return render_template("index.html")
+    if request.method == "GET":
+        db = DatabaseOperations()
+        db.create_tables()
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+
 
 if __name__ == '__main__':
     port = app.config.get("PORT", 5000)
