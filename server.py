fa46a15e4b7a65d826dbdb857b7e3053f6e1d963
saykara <utkuanilsaykara@gmail.com>
2018-12-23 16:02:27 +0300

    Added flask framework and index page of project

--- server.py
+++ server.py
@@ -1,1 +1,13 @@
-#Hello world
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/',  methods=['GET', 'POST'])
+def main_page():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    port = app.config.get("PORT", 5000)
+    debug = True
+    app.run(host='0.0.0.0', port=port, debug=debug)
