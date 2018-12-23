696a3a44f312423051588f005bb442a428acb161
saykara <saykara@itu.edu.tr>
2018-12-23 17:29:36 +0300

    Added excel script to homepage

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from database import DatabaseOperations
+from excel_to_sql import ExcelOperations
 app = Flask(__name__)
 
 
@@ -8,6 +9,8 @@ def main_page():
     if request.method == "GET":
         db = DatabaseOperations()
         db.create_tables()
+        excel = ExcelOperations()
+        excel.transfer()
         return render_template("index.html")
     else:
         return render_template("index.html")
