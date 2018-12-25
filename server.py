e26f428e982381cbb5262f79644566d1707d6208
saykara <saykara@itu.edu.tr>
2018-12-26 02:46:31 +0300

    Arranged pages

--- server.py
+++ server.py
@@ -1,19 +1,44 @@
 from flask import Flask, render_template, request
 from database import DatabaseOperations
 from excel_to_sql import ExcelOperations
+from cloud import CloudOperations
 app = Flask(__name__)
 
 
 @app.route('/',  methods=['GET', 'POST'])
-def main_page():
+def search_page():
     if request.method == "GET":
-        db = DatabaseOperations()
-        db.create_tables()
-        excel = ExcelOperations()
-        excel.transfer()
-        return render_template("index.html")
+        return render_template("cloud_search.html")
     else:
-        return render_template("index.html")
+        cloud = CloudOperations()
+        diskEnd= request.form['DiskCapacity']
+        ramEnd= request.form['RAM']
+        diskStart = cloud.Get_Disk_Capacity(diskEnd)
+        ramStart = cloud.Get_Ram_Capacity(ramEnd)
+        searchList = cloud.Search_Cloud(region=request.form['Region'], os=request.form['OS'], core=request.form['Core'],
+                                        diskType=request.form['DiskType'], diskCapStart=diskStart, diskCapEnd=int(diskEnd),
+                                        ramStart=ramStart, ramEnd=int(ramEnd))
+        return result_page(searchList)
+
+
+@app.route('/result',  methods=['GET', 'POST'])
+def result_page(searchList):
+    return render_template("cloud_search.html", searchList=searchList)
+
+
+@app.route('/initdb')
+def init_page():
+    db = DatabaseOperations()
+    db.create_tables()
+    return "Database initialized!"
+
+
+@app.route('/excel')
+def excel_page():
+    excel = ExcelOperations()
+    excel.transfer()
+    return "Data was transferred to the database!"
+
 
 
 
