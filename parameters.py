052baabc6a185c91b497a7d055bd467e01a17e09
saykara <saykara@itu.edu.tr>
2019-06-14 07:21:01 +0300

    Parameter file added

---
+++ parameters.py
@@ -0,0 +1,11 @@
+class CloudParameters:
+    region = ["US East", "Europe West", "US West", "Asia Pacific", "US Central"]
+    operating_system = ["Windows", "Ubuntu"]
+    brand = ["Google", "Azure", "Amazon"] #, "Ali Baba"
+    storage_type = ["SSD", "HDD"]
+    storage_capacity = [128, 256, 512, 1024, 2048, 4096]
+    cpu = [1, 2, 4, 8]
+    ram = {'1': [],
+           '2': [],
+           '4': [],
+           '8': []}
